f031e5f71385ebb398e65b285829ed6c849622af
lipeedutra <luis.dutra.001@acad.pucrs.br>
2019-12-19 21:31:44 -0300

    First commit

---
+++ PersonAge/PersonAge/PersonAge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonAge
+{
+    public class PersonAge
+    {
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age(DateTime date)
+        {
+            var timeSpan = DateTime.Today - DateOfBirth;
+            var years = timeSpan.Days / 365;
+            
+            return years;
+        }
+    }
+}
---
+++ PersonAge/PersonAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send the year: ");
+            var year = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Send the month: ");
+            var month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Send the day: ");
+            var day = Convert.ToInt32(Console.ReadLine());
+
+            PersonAge personAge = new PersonAge();
+            var age = personAge.DateOfBirth = new DateTime(year, month, day);
+
+            int result = personAge.Age(age);
+
+            Console.WriteLine("You have " + result + " years old :)");
+        }
+    }
+}
